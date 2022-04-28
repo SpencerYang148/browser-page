5a731e6f3d98e7930456007be87db417da991f68
yangshibin <yangshibin@cvte.com>
2022-04-28 14:57:04 +0800

    添加tree shaking

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     module: {
         rules: [
             {
+                sideEffects: false,
                 test: /\.tsx?$/,
                 loader: 'ts-loader',
                 options: { configFile: path.resolve(process.cwd(), 'tsconfig.json') },
